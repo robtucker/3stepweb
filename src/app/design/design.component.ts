4a4841b21a92bbf839c40eec86d72041d08b01ee
robtucker <robtucker@users.noreply.github.com>
2016-10-20 19:15:49 +0100

    adds development page

--- src/app/design/design.component.ts
+++ src/app/design/design.component.ts
@@ -3,30 +3,30 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 import { Logger } from "../core";
 
 @Component({
-  selector: 'design',
-  templateUrl: './design.template.html'
+    selector: 'design',
+    templateUrl: './design.template.html'
 })
 export class DesignComponent implements OnInit { 
 
-private playerHeight;
+    private playerHeight;
 
     constructor(
-      private logger: Logger,
-      private elementRef: ElementRef
+        private logger: Logger,
+        private elementRef: ElementRef
     ) {}
     
     ngOnInit(): void {
-      this.logger.debug(this);
-      this.setIframeHeight();
+        this.logger.debug(this);
+        this.setIframeHeight();
     }
 
     setIframeHeight() {
-      this.playerHeight = `${ window.innerWidth * 0.5 }px` ;
+        this.playerHeight = `${ window.innerWidth * 0.5 }px` ;
     }
 
-  onResize(event) {
-    this.logger.debug('resize event received');
-    this.setIframeHeight();
-  }
+    onResize(event) {
+        this.logger.debug('resize event received');
+        this.setIframeHeight();
+    }
 
 }
